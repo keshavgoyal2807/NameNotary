5f4176f1a0780bd577ef97b0c507fe8b057f1c6d
keshavgoyal2807 <keshavgoyal2014@gmail.com>
2020-03-14 22:32:00 +0530

    addedfiles

---
+++ app/src/index.js
@@ -0,0 +1,69 @@
+import Web3 from "web3";
+import starNotaryArtifact from "../../build/contracts/StarNotary.json";
+
+const App = {
+  web3: null,
+  account: null,
+  meta: null,
+
+  start: async function() {
+    const { web3 } = this;
+
+    try {
+      // get contract instance
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = starNotaryArtifact.networks[networkId];
+      this.meta = new web3.eth.Contract(
+        starNotaryArtifact.abi,
+        deployedNetwork.address,
+      );
+
+      // get accounts
+      const accounts = await web3.eth.getAccounts();
+      this.account = accounts[0];
+    } catch (error) {
+      console.error("Could not connect to contract or chain.");
+    }
+  },
+
+  setStatus: function(message) {
+    const status = document.getElementById("status");
+    status.innerHTML = message;
+  },
+
+  createStar: async function() {
+    const { createStar } = this.meta.methods;
+    const name = document.getElementById("Name").value;
+    const id = document.getElementById("Id").value;
+    await createStar(name, id).send({from: this.account});
+    App.setStatus("New Name Owner is " + this.account + ".");
+  },
+
+  // Implement Task 4 Modify the front end of the DAPP
+  lookUp: async function (){
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const id = document.getElementById("lookid").value;
+    let id1 = parseInt(id);
+    let name =  await lookUptokenIdToStarInfo(id1).call({from: this.account})
+    console.log(name);
+    App.setStatus("Name is " + name + ".");
+    
+  }
+
+};
+
+window.App = App;
+
+window.addEventListener("load", async function() {
+  if (window.ethereum) {
+    // use MetaMask's provider
+    App.web3 = new Web3(window.ethereum);
+    await window.ethereum.enable(); // get permission to access accounts
+  } else {
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
+    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
+  }
+
+  App.start();
+});
